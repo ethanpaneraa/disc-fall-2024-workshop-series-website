dce96ec33d791750a6adaf5f5245565ffa898f9b
amyzliao <amzhliao@gmail.com>
2024-09-30 11:56:29 -0500

    disc logo home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,25 @@ import { buttonVariants } from "@/components/ui/button";
 import { page_routes } from "@/lib/routes-config";
 import { MoveUpRightIcon, TerminalIcon } from "lucide-react";
 import Link from "next/link";
+// import discLogo from "@/public/disco-logo.svg";
 
 export default function Home() {
   return (
     <div className="flex sm:min-h-[91vh] min-h-[88vh] flex-col items-center justify-center text-center px-2 py-8">
-      <Link
+      {/* <Link
         href=""
         target="_blank"
         className="mb-5 sm:text-lg flex items-center gap-2 underline underline-offset-4"
       >
         Follow our Mini Lectures{" "}
         <MoveUpRightIcon className="w-4 h-4 font-extrabold" />
-      </Link>
+      </Link> */}
+      <div>
+        <img src={"/disc-logo.png"} alt="disco logo" className="w-96 h-auto mb-3" />
+        <h2 className="text-sm mb-8 sm:text-xl font-bold text-[#23ffe3]">Discover & Innovate for Social Change</h2>
+      </div>
       <h1 className="text-xl font-bold mb-4 sm:text-6xl">
-        Develop & Innovate for Social Change Fall 2024 Workshop Series
+        Fall 2024 Workshop Series
       </h1>
       <p className="mb-8 sm:text-xl max-w-[800px] text-muted-foreground">
         This is a workshop series that will introduce you to the essentials of
